892bc9ef5e423153d722305ed052d302856b7f7e
Jake Bruin <93625099+JakeBruin111@users.noreply.github.com>
2022-01-13 15:08:51 +0000

    Add files via upload

---
+++ Game Engine Group Project/Unity Folder/Scripts/Buttons/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onStart()
+    {
+        //loads up following scene when executed.
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void onSettings()
+    {
+        //loads up following scene when executed.
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void onBack()
+    {
+        //loads up following scene when executed.
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void CloseGame()
+    {
+        //for testing
+        //Debug.Log("Closing");
+
+        //closes the game
+        Application.Quit();
+    }
+}
